6764b258ad575d671dadb4fda7c72f50cf290161
Kingmoh <rashidmincan@gmail.com>
2023-10-22 17:31:43 +1100

    Initial commit

---
+++ MonkeyFinder/Services/MonkeyService.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http.Json;
+
+namespace MonkeyFinder.Services;
+
+public class MonkeyService
+{
+    HttpClient httpClient;
+
+    public MonkeyService()
+    {
+        httpClient = new HttpClient();
+    }
+
+    List<Monkey> monkeyList = new List<Monkey>();
+
+    public async Task<List<Monkey>> GetMonkeys()
+    {
+        if (monkeyList?.Count > 0)
+            return monkeyList;
+
+        string url = "https://raw.githubusercontent.com/jamesmontemagno/app-monkeys/master/MonkeysApp/monkeydata.json";
+        var response = await httpClient.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            // read from the JSON and deserialize as a list of monkeys
+            monkeyList = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+        }
+        return monkeyList;
+    }
+}
